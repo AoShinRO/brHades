689b2c5bbdd79c01cde9f9612a0400e90e0dc023
AoShinHo <126742159+AoShinRO@users.noreply.github.com>
2024-11-03 11:51:57 -0300

    Tradutorcompilado (#137)

--- db/translated/database_pt.lua
+++ db/translated/database_pt.lua
@@ -1,9 +1,18 @@
 ! - Information -!|pt|!- Informação -!
+- Lu just walked away with a cynical look on his face.|pt|- Lu apenas se afastou com um olhar cínico no rosto.
+.....|pt|.....
+....So?|pt|....Então?
 Don't forget to ^4d4dffget some Lumber^000000 for the ship.|pt|Não se esqueça de ^4d4dffget some Lumber^000000 para o navio.
 Don't forget to raise your ^4d4dffskills^000000.|pt|Não se esqueça de aumentar seu ^4d4dffskills^000000.
+From now on I'll be sailing the ship around the island.|pt|A partir de agora estarei navegando no navio ao redor da ilha.
 Good luck!|pt|Boa sorte!
+I am ggjfgh!|pt|Eu sou ggjfgh!
 Kill Porings and bring the Lumber to the crew.|pt|Mate os poros e leve a madeira para a tripulação.
+Let's go, shall we?|pt|Vamos lá, vamos?
+Looks like... he has a shy personality.|pt|Parece ... ele tem uma personalidade tímida.
+Should I introduce myself?:My name is ~!|pt|Devo me apresentar ?: Meu nome é ~!
 The status window can be opened and closed with the shortcut Alt + A^000000.|pt|A janela de status pode ser aberta e fechada com o atalho Alt + A^000000.
+When you go to Izlude, will you check on him?|pt|Quando você for para Izlude, você vai verificar ele?
 ^4d4dffPlease properly allocate the stats you need to battle^000000.|pt|^4d4dffPlease properly allocate the stats you need to battle^000000.
 ^4d4dffThe Ship^000000 is anchored in the southwest. It will take you to Izlude.|pt|^4d4dffThe Ship^000000 está ancorado no sudoeste.Isso o levará a Izlude.
 ^4d4dffYou get a stats bonus for every level you raise.|pt|^4d4dffffyou um bônus de estatísticas para todos os níveis que você aumenta.
