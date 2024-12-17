2982839b0f2203d9a8ec38c561c6ebf3ec2ee1c4
AoShinRO <darklord.dl@gmail.com>
2024-12-17 17:59:09 -0300

    correção_showcostume_clientantigos

--- src/map/pc.cpp
+++ src/map/pc.cpp
@@ -15329,7 +15329,11 @@ void pc_set_costume_view(map_session_data *sd) {
 		sd->status.robe = id->look;
 
 	// Costumes check
-	if (!map_getmapflag(sd->bl.m, MF_NOCOSTUME) && !sd->status.show_costumes) {
+	if (!map_getmapflag(sd->bl.m, MF_NOCOSTUME)
+#if PACKETVER_MAIN_NUM >= 20230921 || PACKETVER_RE_NUM >= 20230906
+		&& !sd->status.show_costumes
+#endif
+		) {
 		if ((i = sd->equip_index[EQI_COSTUME_HEAD_LOW]) != -1 && (id = sd->inventory_data[i])) {
 			if (!(id->equip&(EQP_COSTUME_HEAD_MID|EQP_COSTUME_HEAD_TOP)))
 				sd->status.head_bottom = id->look;
