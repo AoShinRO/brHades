e58cd4d718a56be8c65a6ee15db4533f5597c11b
KennyGeek <kenny.zapdos1@gmail.com>
2025-05-23 20:21:56 -0300

    Update core.hpp

--- src/config/core.hpp
+++ src/config/core.hpp
@@ -114,7 +114,7 @@
 
 /// Descomente para habilitar o uso de APIs da google para traducao de npcs com o atcommand @langtype. PS: Isso pode ser lento...
 /// Python 3 needed
-//#define TRANSLATION_API
+#define TRANSLATION_API
 
 #if defined(TRANSLATION_API)
 	/// Descomente para usar o translator.py (python script not-builded) na pasta tools/. python 3 requerido, para fins de testes, mais lento que buildado
